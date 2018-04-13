4519c04357885bd17eac1c8ea86537db3f403452
TERcrash <TERcrash@users.noreply.github.com>
2018-04-13 19:50:21 +0300

    kek x2

---
+++ 37/football/Assets/Script/KickTheBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickTheBall : MonoBehaviour {
+
+    public Transform ball;
+    public float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(ball.position, transform.position);
+        if (dist < 2 && Input.GetKeyDown(KeyCode.Mouse0)) {
+            Rigidbody rb = ball.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward*speed;
+        }
+        if(dist < 1) {
+            Rigidbody rb = ball.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * speed*0.2f;
+        }
+    }
+}
