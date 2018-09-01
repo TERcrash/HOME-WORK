851de77b5453833975f1aa9f174372620d4723b2
TERcrash <TERcrash@users.noreply.github.com>
2018-09-01 17:59:18 +0300

    вот запушил

---
+++ 43/test/test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Task.PrintColoredText("This text should be red", ConsoleColor.Red);
+        Task.PrintColoredText("This text should be yellow", ConsoleColor.Yellow);
+        Console.Read();
+    }
+}
+
+class Task {
+    public static void PrintColoredText(string text, ConsoleColor color) {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+    }
+}
