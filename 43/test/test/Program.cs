594a801b4fbf57737dcb536125f680f83146f830
TERcrash <TERcrash@users.noreply.github.com>
2018-09-22 19:44:38 +0300

    lul

--- 43/test/test/Program.cs
+++ 43/test/test/Program.cs
@@ -1,19 +1,36 @@
 ﻿using System;
 
 class Program {
-    static void Main(string[] args) {
-        Task.PrintAsterisks(3);
-        Console.Read();
+    static void Main() {
+        string mostWidespread = Task.GetMostWidespread(new[] { "Emperor Penguin", "Macaroni Penguin", "Emperor Penguin", "Little Penguin" });
+        Console.WriteLine(mostWidespread); // Emperor Penguin
     }
 }
 
-
 class Task {
-    public static void PrintAsterisks(int amount) {
-        string asterisks = "";
-        for (int i = 0; i < amount; i++) {
-            asterisks = asterisks + "*";
+    int EmperorPenguins = 0;
+    int LittlePenguins = 0;
+    int MacaroniPenguins = 0;
+    public static String GetMostWidespread() {
+        for (int i = 0; i < mostWidespread; i++) {
+            if (mostWidespread[i] = "Emperor Penguins") {
+                EmperorPenguins = EmperorPenguins + 1;
+            }
+
+            if (mostWidespread[i] = "Little Penguins") {
+                LittlePenguins = LittlePenguins + 1;
+            }
+
+            if (mostWidespread[i] = "Macaroni Penguins") {
+                MacaroniPenguins = MacaroniPenguins + 1;
+            }
         }
-        Console.WriteLine(asterisks);
+        if (EmperorPenguins > EmperorPenguins && EmperorPenguins > MacaroniPenguins) {
+            return EmperorPenguins;
+        } else if (LittlePenguins > EmperorPenguins && LittlePenguins > MacaroniPenguins) {
+            return LittlePenguins;
+        } else {
+            return MacaroniPenguins;
+        }
     }
 }
